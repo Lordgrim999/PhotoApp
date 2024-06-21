67d65b403158f21b074206edcfb537786f76d953
Lordgrim999 <srivastavaak47rishi@gmail.com>
2024-06-21 18:37:02 +0530

    changes for method in AuthenticationFilter

--- photo-app-users/src/main/java/com/tlecoders/photoapp/photo_app_users/security/AuthenticationFilter.java
+++ photo-app-users/src/main/java/com/tlecoders/photoapp/photo_app_users/security/AuthenticationFilter.java
@@ -2,12 +2,15 @@ package com.tlecoders.photoapp.photo_app_users.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tlecoders.photoapp.photo_app_users.model.LoginRequestModel;
+import com.tlecoders.photoapp.photo_app_users.service.UsersService;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -15,11 +18,13 @@ import java.util.ArrayList;
 
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    public AuthenticationFilter(AuthenticationManager authenticationManager)
+    private UsersService usersService;
+    public AuthenticationFilter(AuthenticationManager authenticationManager,UsersService usersService)
     {
+
         // adding the authentication manager to Super class
         super(authenticationManager);
+        this.usersService=usersService;
     }
 
     @Override
@@ -34,6 +39,13 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult)
+    {
+        User userObject = (User) authResult.getPrincipal();
+        String username = userObject.getUsername();
 
     }
 
